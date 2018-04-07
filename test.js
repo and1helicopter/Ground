16dedc9f842343b59d8a1c9b6b7b205d40755dfc
Denis <and1helicopter@mail.ru>
2018-04-07 13:19:59 +0300

    v0.0a.0.01
    +Radar view

---
+++ test.js
@@ -0,0 +1,76 @@
+
+function show_info (alg){
+    var str = '';
+    if(alg === "azimuth"){
+        str = new String(azimuth_index * 8  ) + '-' + new String(azimuth_index + 1* 8);
+    }
+    document.getElementById("label_angel").disabled = true;
+}
+
+if(azimuth_index === 0) document.getElementById("kartin").src = 'img/zone_01.jpg';
+if(azimuth_index === 1) document.getElementById("kartin").src = 'img/zone_02.jpg';
+if(azimuth_index === 2) document.getElementById("kartin").src = 'img/zone_03.jpg';
+if(azimuth_index === 3) document.getElementById("kartin").src = 'img/zone_04.jpg';
+if(azimuth_index === 4) document.getElementById("kartin").src = 'img/zone_05.jpg';
+if(azimuth_index === 5) document.getElementById("kartin").src = 'img/zone_06.jpg';
+if(azimuth_index === 6) document.getElementById("kartin").src = 'img/zone_07.jpg';
+if(azimuth_index === 7) document.getElementById("kartin").src = 'img/zone_08.jpg';
+if(azimuth_index === 8) document.getElementById("kartin").src = 'img/zone_09.jpg';
+if(azimuth_index === 9) document.getElementById("kartin").src = 'img/zone_10.jpg';
+if(azimuth_index === 10) document.getElementById("kartin").src = 'img/zone_11.jpg';
+if(azimuth_index === 11) document.getElementById("kartin").src = 'img/zone_12.jpg';
+if(azimuth_index === 12) document.getElementById("kartin").src = 'img/zone_13.jpg';
+if(azimuth_index === 13) document.getElementById("kartin").src = 'img/zone_14.jpg';
+if(azimuth_index === 14) document.getElementById("kartin").src = 'img/zone_15.jpg';
+if(azimuth_index === 15) document.getElementById("kartin").src = 'img/zone_16.jpg';
+
+else if(azimuth_index === 16) document.getElementById("kartin").src = 'img/zone_17.jpg';
+else if(azimuth_index === 17) document.getElementById("kartin").src = 'img/zone_18.jpg';
+else if(azimuth_index === 18) document.getElementById("kartin").src = 'img/zone_19.jpg';
+else if(azimuth_index === 19) document.getElementById("kartin").src = 'img/zone_20.jpg';
+else if(azimuth_index === 20) document.getElementById("kartin").src = 'img/zone_21.jpg';
+else if(azimuth_index === 21) document.getElementById("kartin").src = 'img/zone_22.jpg';
+else if(azimuth_index === 22) document.getElementById("kartin").src = 'img/zone_23.jpg';
+else if(azimuth_index === 23) document.getElementById("kartin").src = 'img/zone_24.jpg';
+else if(azimuth_index === 24) document.getElementById("kartin").src = 'img/zone_25.jpg';
+else if(azimuth_index === 25) document.getElementById("kartin").src = 'img/zone_26.jpg';
+else if(azimuth_index === 26) document.getElementById("kartin").src = 'img/zone_27.jpg';
+else if(azimuth_index === 27) document.getElementById("kartin").src = 'img/zone_28.jpg';
+else if(azimuth_index === 28) document.getElementById("kartin").src = 'img/zone_29.jpg';
+else if(azimuth_index === 29) document.getElementById("kartin").src = 'img/zone_30.jpg';
+else if(azimuth_index === 30) document.getElementById("kartin").src = 'img/zone_31.jpg';
+else if(azimuth_index === 31) document.getElementById("kartin").src = 'img/zone_32.jpg';
+
+else if(azimuth_index === 32) document.getElementById("kartin").src = 'img/zone_33.jpg';
+else if(azimuth_index === 33) document.getElementById("kartin").src = 'img/zone_34.jpg';
+else if(azimuth_index === 34) document.getElementById("kartin").src = 'img/zone_35.jpg';
+else if(azimuth_index === 35) document.getElementById("kartin").src = 'img/zone_36.jpg';
+else if(azimuth_index === 36) document.getElementById("kartin").src = 'img/zone_37.jpg';
+else if(azimuth_index === 37) document.getElementById("kartin").src = 'img/zone_38.jpg';
+else if(azimuth_index === 38) document.getElementById("kartin").src = 'img/zone_39.jpg';
+else if(azimuth_index === 39) document.getElementById("kartin").src = 'img/zone_40.jpg';
+else if(azimuth_index === 40) document.getElementById("kartin").src = 'img/zone_41.jpg';
+else if(azimuth_index === 41) document.getElementById("kartin").src = 'img/zone_42.jpg';
+else if(azimuth_index === 42) document.getElementById("kartin").src = 'img/zone_43.jpg';
+else if(azimuth_index === 43) document.getElementById("kartin").src = 'img/zone_44.jpg';
+else if(azimuth_index === 44) document.getElementById("kartin").src = 'img/zone_45.jpg';
+else if(azimuth_index === 45) document.getElementById("kartin").src = 'img/zone_46.jpg';
+else if(azimuth_index === 46) document.getElementById("kartin").src = 'img/zone_47.jpg';
+else if(azimuth_index === 47) document.getElementById("kartin").src = 'img/zone_48.jpg';
+else if(azimuth_index === 48) document.getElementById("kartin").src = 'img/zone_49.jpg';
+else if(azimuth_index === 49) document.getElementById("kartin").src = 'img/zone_40.jpg';
+else if(azimuth_index === 50) document.getElementById("kartin").src = 'img/zone_51.jpg';
+else if(azimuth_index === 51) document.getElementById("kartin").src = 'img/zone_52.jpg';
+else if(azimuth_index === 52) document.getElementById("kartin").src = 'img/zone_53.jpg';
+else if(azimuth_index === 53) document.getElementById("kartin").src = 'img/zone_54.jpg';
+else if(azimuth_index === 54) document.getElementById("kartin").src = 'img/zone_55.jpg';
+else if(azimuth_index === 55) document.getElementById("kartin").src = 'img/zone_56.jpg';
+else if(azimuth_index === 56) document.getElementById("kartin").src = 'img/zone_57.jpg';
+else if(azimuth_index === 57) document.getElementById("kartin").src = 'img/zone_58.jpg';
+else if(azimuth_index === 58) document.getElementById("kartin").src = 'img/zone_59.jpg';
+else if(azimuth_index === 59) document.getElementById("kartin").src = 'img/zone_60.jpg';
+else if(azimuth_index === 60) document.getElementById("kartin").src = 'img/zone_61.jpg';
+else if(azimuth_index === 61) document.getElementById("kartin").src = 'img/zone_62.jpg';
+else if(azimuth_index === 62) document.getElementById("kartin").src = 'img/zone_63.jpg';
+else if(azimuth_index === 63) document.getElementById("kartin").src = 'img/zone_64.jpg';
+
